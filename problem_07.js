1ad7dffe404e8d2baa9c123584ff0536cdb58ac5
Zahidul Islam <zahidul670791@gmail.com>
2025-10-07 22:21:54 -0700

    problem_07 slove

---
+++ problem_07.js
@@ -0,0 +1,42 @@
+
+// problem_047
+
+// Write a program that prints each character of a string on a new line.
+// Hint: Use a for loop to go through the string.
+
+// Sample Input: "Code"
+// Sample Output:
+
+// C
+// o
+// d
+// e
+
+
+
+// slove_01
+
+let str = "Code";
+
+for(let i =0; i < str.length; i++){
+    console.log(str[i]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// slove_02
+// let str = "Code";
+
+// for(let char of str){
+//     console.log(char);
+// }
